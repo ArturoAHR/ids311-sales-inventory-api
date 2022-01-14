a27532d718df4b48534305a67a2e5d860324b220
RukShu <arturoaureliohr@gmail.com>
2022-01-14 14:48:03 -0400

    Fix TypeORM relations in imports

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Item } from 'src/entities/item.entity';
+import { Payment } from 'src/entities/payment.entity';
+import { Sale } from 'src/entities/sale.entity';
 import { UserRepository } from 'src/repositories/user.repository';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [TypeOrmModule.forFeature([UserRepository, Payment, Sale, Item])],
   controllers: [UserController],
   providers: [UserService],
 })
