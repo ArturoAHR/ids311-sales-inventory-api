9f1fc63374d5154468452ce984afebf54804bc4b
RukShu <arturoaureliohr@gmail.com>
2022-01-14 17:47:08 -0400

    Implement Controller and Item CRUD functions

---
+++ src/modules/item/item.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ItemDTO } from 'src/dto/item.dto';
+import { Item } from 'src/entities/item.entity';
+import { ItemService } from './item.service';
+
+@Controller('item')
+export class ItemController {
+  constructor(private readonly itemService: ItemService) {}
+
+  @Get()
+  async getItems(): Promise<Item[]> {
+    return await this.itemService.getItems();
+  }
+
+  @Post()
+  async createItem(@Body() itemDto: ItemDTO): Promise<Item> {
+    return await this.itemService.createItem(itemDto);
+  }
+
+  @Get('/:id')
+  async getItem(@Param('id') id: string): Promise<Item> {
+    return await this.itemService.getItem(id);
+  }
+
+  @Put('/:id')
+  async updateItem(
+    @Param('id') id: string,
+    @Body() itemDto: ItemDTO,
+  ): Promise<Item> {
+    itemDto.id = id;
+    return await this.itemService.updateItem(itemDto);
+  }
+
+  @Delete('/:id')
+  async deleteItem(@Param('id') id: string): Promise<Item> {
+    return await this.itemService.deleteItem(id);
+  }
+}
