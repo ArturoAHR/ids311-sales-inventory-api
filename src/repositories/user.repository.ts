47e01aae3626b6007f41ac657c52d391eab2e20b
RukShu <arturoaureliohr@gmail.com>
2022-01-14 17:22:22 -0400

    Implement User CRUD functions in entity repository

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,9 +1,53 @@
+import { randomUUID } from 'crypto';
+import { UserDTO } from 'src/dto/user.dto';
+import { assign } from 'src/utils/object-manipulation';
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from '../entities/user.entity';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async getAll(): Promise<User[]> {
-    return await this.createQueryBuilder('usuarios').getMany();
+    return await this.createQueryBuilder('users')
+      .where('users.fechaEliminacion is null')
+      .getMany();
+  }
+
+  async createUser(userDto: UserDTO): Promise<User> {
+    userDto.id = randomUUID();
+    const user = new User();
+    assign(user, userDto);
+    await this.save(user);
+    return user;
+  }
+
+  async getUser(id: string) {
+    try {
+      return await this.findOneOrFail({
+        where: { id: id, deletionDate: null },
+      });
+    } catch (error) {
+      //log
+      throw error;
+    }
+  }
+
+  async updateUser(userDto: UserDTO): Promise<User> {
+    try {
+      const user = await this.getUser(userDto.id);
+      assign(user, userDto);
+      await this.save(user);
+      return user;
+    } catch (error) {
+      //log
+      throw error;
+    }
+  }
+
+  async deleteUser(id: string): Promise<User> {
+    const userDto = new UserDTO();
+    userDto.id = id;
+    userDto.deletionDate = new Date();
+    console.log(userDto);
+    return await this.updateUser(userDto);
   }
 }
