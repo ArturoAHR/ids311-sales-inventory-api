85ac6317683cb3bfd46f0746cad8691841aa1f31
RukShu <arturoaureliohr@gmail.com>
2022-01-14 14:45:49 -0400

    Fix TypeORM Config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,22 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import config from './config/ormconfig';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     UserModule,
     TypeOrmModule.forRoot({
-      ...config,
+      type: 'postgres',
+      host: process.env.TYPEORM_HOST,
+      port: Number(process.env.TYPEORM_PORT),
+      username: process.env.TYPEORM_USERNAME,
+      password: process.env.TYPEORM_PASSWORD,
+      database: process.env.TYPEORM_DATABASE,
+      entities: [process.env.TYPEORM_ENTITIES],
+      cli: {
+        migrationsDir: process.env.TYPEORM_MIGRATIONS,
+      },
       autoLoadEntities: true,
       synchronize: true,
     }),
