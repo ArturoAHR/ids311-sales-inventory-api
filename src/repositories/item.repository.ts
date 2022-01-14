cee2c38e7f4ab1e0ddb91112f8ec3e0adb17514d
RukShu <arturoaureliohr@gmail.com>
2022-01-14 17:48:06 -0400

    Implement Item Repository and CRUD Operations

---
+++ src/repositories/item.repository.ts
@@ -0,0 +1,53 @@
+import { randomUUID } from 'crypto';
+import { ItemDTO } from 'src/dto/item.dto';
+import { assign } from 'src/utils/object-manipulation';
+import { EntityRepository, Repository } from 'typeorm';
+import { Item } from '../entities/item.entity';
+
+@EntityRepository(Item)
+export class ItemRepository extends Repository<Item> {
+  async getAll(): Promise<Item[]> {
+    return await this.createQueryBuilder('items')
+      .where('items.fechaEliminacion is null')
+      .getMany();
+  }
+
+  async createItem(itemDto: ItemDTO): Promise<Item> {
+    itemDto.id = randomUUID();
+    const item = new Item();
+    assign(item, itemDto);
+    await this.save(item);
+    return item;
+  }
+
+  async getItem(id: string) {
+    try {
+      return await this.findOneOrFail({
+        where: { id: id, deletionDate: null },
+      });
+    } catch (error) {
+      //log
+      throw error;
+    }
+  }
+
+  async updateItem(itemDto: ItemDTO): Promise<Item> {
+    try {
+      const item = await this.getItem(itemDto.id);
+      assign(item, itemDto);
+      await this.save(item);
+      return item;
+    } catch (error) {
+      //log
+      throw error;
+    }
+  }
+
+  async deleteItem(id: string): Promise<Item> {
+    const itemDto = new ItemDTO();
+    itemDto.id = id;
+    itemDto.deletionDate = new Date();
+    console.log(itemDto);
+    return await this.updateItem(itemDto);
+  }
+}
