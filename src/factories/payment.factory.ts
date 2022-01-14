9dae68019b1ec4adfc24e22c1db50b2769a608cb
RukShu <arturoaureliohr@gmail.com>
2022-01-14 14:46:34 -0400

    Add Factories for Users, Items, Payments and Sales

---
+++ src/factories/payment.factory.ts
@@ -0,0 +1,17 @@
+export const EXAMPLE_PAYMENTS = [
+  {
+    id: '0faa9d6f-5d7e-4a67-a6d3-5cd248020533',
+    user: 'f8e1e330-8383-4de6-a97e-ff2d57cf5d7b',
+    amount: 3.98,
+  },
+  {
+    id: '2d86db1b-db6d-4625-a2cb-cb7289dfae09',
+    user: 'db1ba15f-8c78-438a-a521-6b11e73ed99f',
+    amount: 9.99,
+  },
+  {
+    id: '4445fb0e-1722-4c85-918f-06880ff68926',
+    user: 'db1ba15f-8c78-438a-a521-6b11e73ed99f',
+    amount: 31.98,
+  },
+];
---
+++ src/factories/user.factory.ts
@@ -0,0 +1,26 @@
+export const EXAMPLE_USERS = [
+  {
+    id: 'c9940b42-6579-4390-b079-259e28efd846',
+    firstName: 'José',
+    lastName: 'Pérez',
+    birthDate: new Date('12-09-1943'),
+  },
+  {
+    id: 'f8e1e330-8383-4de6-a97e-ff2d57cf5d7b',
+    firstName: 'María',
+    lastName: 'Solano',
+    birthDate: new Date('01-09-1976'),
+  },
+  {
+    id: 'db1ba15f-8c78-438a-a521-6b11e73ed99f',
+    firstName: 'Juan',
+    lastName: 'Mendez',
+    birthDate: new Date('10-03-1967'),
+  },
+  {
+    id: '4730964d-7acf-4a19-9eb6-04a236e81365',
+    firstName: 'Mariela',
+    lastName: 'Lugo',
+    birthDate: new Date('11-05-2000'),
+  },
+];
