89656e2c82a8ef7bfed2e04be01de5a5c323d38b
RukShu <arturoaureliohr@gmail.com>
2022-01-14 20:39:24 -0400

    Implement Create User Payment Function

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,12 +3,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Item } from 'src/entities/item.entity';
 import { Payment } from 'src/entities/payment.entity';
 import { Sale } from 'src/entities/sale.entity';
+import { ItemRepository } from 'src/repositories/item.repository';
+import { PaymentRepository } from 'src/repositories/payment.repository';
+import { SaleRepository } from 'src/repositories/sale.repository';
 import { UserRepository } from 'src/repositories/user.repository';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository, Payment, Sale, Item])],
+  imports: [
+    TypeOrmModule.forFeature([
+      UserRepository,
+      PaymentRepository,
+      Payment,
+      SaleRepository,
+      Sale,
+      ItemRepository,
+      Item,
+    ]),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
